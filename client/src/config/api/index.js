41a30407994429cca09b49e79d7e03b7ee2254b4
Klim <klim@rva.solutions>
2023-01-23 23:48:41 +0200

    update api host

--- client/src/config/api/index.js
+++ client/src/config/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api/v1';// 'https://dalle-arbb.onrender.com/api/v1/dalle';
+const API_URL = 'https://dall-e-hosm.onrender.com';// 'https://dalle-arbb.onrender.com/api/v1/dalle';
 
 export const axios_api = axios.create({
   baseURL: API_URL,
